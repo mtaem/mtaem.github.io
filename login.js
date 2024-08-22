821d5baf8aea85063cec1ed6a4bfbee7855188cd
Li-xinze76 <139776917+Li-xinze76@users.noreply.github.com>
2024-08-22 17:54:49 +0800

    con

--- login.js
+++ login.js
@@ -1,35 +1,21 @@
 document.getElementById('login-btn').addEventListener('click', function() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-    
-    // 发送 AJAX 请求
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'users.json', true);
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhr.send(JSON.stringify({ username: username, password: password }));
 
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            var users = JSON.parse(xhr.responseText).users;
-            var user = users.find(u => u.username === username && u.password === password);
-
+    fetch('users.json')
+        .then(response => response.json())
+        .then(data => {
+            var user = data.users.find(user => user.username === username && user.password === password);
             if (user) {
                 if (user.role === 'banned') {
                     showTooltip('此用户已封禁');
                 } else {
-                    // 设置 cookie
-                    var expiryDate = new Date();
-                    expiryDate.setTime(expiryDate.getTime() + (24 * 60 * 60 * 1000)); // 1天后过期
-                    var expires = "expires=" + expiryDate.toUTCString();
-                    document.cookie = "username=" + encodeURIComponent(username) + "; " + expires + "; path=/";
-                    // 重定向到首页
+                    document.cookie = "username=" + encodeURIComponent(username) + "; path=/";
                     window.location.href = 'index.html';
                 }
             } else {
                 showTooltip('用户名或密码不正确');
             }
-        } else {
-            showTooltip('请求失败，请稍后再试');
-        }
-    };
+        })
+        .catch(error => console.error('请求失败:', error));
 });
